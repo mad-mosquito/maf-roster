f34c1d4cf151d1f8376f88deb3e97d8b135d1be6
des <skoonta@gmail.com>
2017-09-19 22:03:08 +1200

    update openshit v3

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+        self.ipaddress = '0.0.0.0';
+        self.port      = 8080 || 3000;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -147,7 +147,7 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         //self.createRoutes();
-        self.app = express.createServer();
+        self.app = express();
 
         //Add handlers for the app (from the routes).
         //for (var r in self.routes) {
