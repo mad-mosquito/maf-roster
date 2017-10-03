d213b3bb8d1cc806087266f808ee5bc0f5290185
des <skoonta@gmail.com>
2017-10-04 11:49:11 +1300

    'asdfasdfasdf'

--- mongo.js
+++ mongo.js
@@ -26,7 +26,9 @@ module.exports = {
 			var mongo_url = 'mongodb://admin:qshIrs8mFvqq@' + mongo_ip + ':' + mongo_port + '/node'
 */
 		//var mongo_url = 'mongodb://user7IN:VmssRcvIjOY2lrcv@mongodb/roster_db'
-		var mongo_url = 'mongodb://user7IN:VmssRcvIjOY2lrcv@mongodb/node'
+		//var mongo_url = 'mongodb://user7IN:VmssRcvIjOY2lrcv@mongodb/node'
+		var mongo_url = 'mongodb://admin:780GWkD50g0Uicp4@mongodb/node'
+
 
 		
 		MongoClient.connect(mongo_url, function(err, db) {
